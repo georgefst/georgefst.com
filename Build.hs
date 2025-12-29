439e8dd7461db1bff259f7bc902e1292d2ff2c72
George Thomas <georgefsthomas@gmail.com>
2025-12-29 17:59:45 +0000

    Simplify notes about one-time release setup
    This version hasn't been tested from scratch, but should work.

--- Build.hs
+++ Build.hs
@@ -92,10 +92,7 @@ main = shakeArgs shakeOpts do
 
     -- TODO requires one-time setup on each dev machine:
     -- git remote add github.io git@github.com:georgefst/georgefst.github.io.git
-    -- git fetch github.io
-    -- git switch georgefst.com
-    -- git switch -c release
-    -- git switch master
+    -- git branch --no-track release github.io/georgefst.com
     "release" ~> do
         alwaysRerun
         liftIO $ removeDirectoryRecursive outDir
